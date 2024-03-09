f6b83187c39567d426a4f9b2458121f2e0b38e15
Ishan Singh <nameste380@gmail.com>
2024-03-09 23:32:15 +0530

    addingLoginUser

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = require("../models/userModel");
 
@@ -35,7 +36,47 @@ module.exports = {
         message: "User registered successfully!",
       });
     } catch (error) {
-      console.error(error);
+      res.status(500).send({
+        success: false,
+        message: "Error!",
+        error: error,
+      });
+    }
+  },
+  loginUser: async (req: Request, res: Response) => {
+    try {
+      const { userEmail, userPassword } = req.body; // ? Extract Data from body
+      const isEmail = await userSchema.findOne({
+        userEmail: userEmail,
+      });
+      if (!isEmail) {
+        return res.status(401).json({
+          success: false,
+          message: "Invalid credentials. User not found.",
+        });
+      }
+      const userData = isEmail;
+      const token: string = jwt.sign({ userData }, "BankProX", {
+        // ? Creating Token with JWT
+        expiresIn: "1h",
+      });
+      const isCorrectPassword: boolean = await bcrypt.compare(
+        // ? Comparing Password
+        userPassword,
+        userData.userPassword
+      );
+      if (!isCorrectPassword) {
+        return res.status(401).send({
+          success: false,
+          message: "Invalid credentials. Password incorrect.",
+        });
+      }
+      res.status(200).send({
+        success: true,
+        message: "Authentication successful.",
+        token: token,
+      });
+    } catch (error) {
       res.status(500).send({
         success: false,
         message: "Error!",
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,5 +5,6 @@ const userController = require("../controllers/userController");
 const userRoute = Router();
 
 userRoute.post("/registerUser", userController.registerUser);
+userRoute.post("/loginUser", userController.loginUser);
 
 module.exports = userRoute;
